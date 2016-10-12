bb0b5ded5f345442933a536251f37edbdda52a07
Robert Weiser <robert.weiser@elca.ch>
2016-10-12 13:15:51 +0100

    Initial project layout

---
+++ app/src/main/java/ch/elca/elcathon/awsiotsample/IotActivity.java
@@ -0,0 +1,13 @@
+package ch.elca.elcathon.awsiotsample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IotActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iot);
+    }
+}
